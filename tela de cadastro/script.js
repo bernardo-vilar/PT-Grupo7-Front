aa39d9e4d6b404c0ddafc8925b6147a10463072a
RafaelMkrauss <rafaelmkrauss@gmail.com>
2024-11-29 13:16:13 -0300

    :tada:
    :tada:

---
+++ tela de cadastro/script.js
@@ -0,0 +1,12 @@
+function lerValor() {
+    var valor = document.getElementById("Nome").value;
+    document.getElementById("resultadoNome").innerText = "O valor da caixa de texto 1 é: " + valor;
+    var valor = document.getElementById("Email").value;
+    document.getElementById("resultadoEmail").innerText = "O valor da caixa de texto 2 é: " + valor;
+    var valor = document.getElementById("Senha").value;
+    document.getElementById("resultadoSenha").innerText = "O valor da caixa de texto 3 é: " + valor;
+    var valor = document.getElementById("Curso").value;
+    document.getElementById("resultadoCurso").innerText = "O valor da caixa de texto 4 é: " + valor;
+    var valor = document.getElementById("Departamento").value;
+    document.getElementById("resultadoDepartamento").innerText = "O valor da caixa de texto 5 é: " + valor;
+}
