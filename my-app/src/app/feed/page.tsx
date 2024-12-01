9c4e88cf621a59718f49001c542671efa19199b4
marcio-guimaraes <mv601200@gmail.com>
2024-12-01 15:14:57 -0300

    Iniciando tela de Feed

---
+++ my-app/src/app/feed/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="">
+      <header className="bg-red-400 flex h-[4rem] items-center justify-center">
+        <h1>Feed</h1>
+      </header>
+    </main>
+  );
+}
