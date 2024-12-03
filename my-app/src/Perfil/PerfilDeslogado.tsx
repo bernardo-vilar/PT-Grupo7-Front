550a0c6f16511b19163b0a1e40aed7a5c255f37f
GabLopes12 <glopess1204@gmail.com>
2024-12-03 10:28:01 -0300

    comecando as telas de perfil

---
+++ my-app/src/Perfil/PerfilDeslogado.tsx
@@ -0,0 +1,7 @@
+
+import HeaderDeslogado from "../components/headerDeslogado";
+
+export default function PerfilDeslogado() {
+  return (
+    <main className="bg-gray-200 h-screen">
+      <HeaderDeslogado />
---
+++ my-app/src/Perfil/PerfilLogado.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import HeaderLogado from '../components/headerLogado'
+
+
+const PerfilLogado = () => {
+  return (
+    <main className="bg-gray-200 h-screen">
+      <HeaderLogado />
