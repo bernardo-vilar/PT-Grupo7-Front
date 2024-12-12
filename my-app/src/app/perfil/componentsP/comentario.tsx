21e7d72dfb50fd2417077ad8b49b4d75bad95c68
RafaelMkrauss <rafaelmkrauss@gmail.com>
2024-12-12 19:48:34 -0300

    Update comentario.tsx

--- my-app/src/app/perfil/componentsP/comentario.tsx
+++ my-app/src/app/perfil/componentsP/comentario.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image"
 import Link from 'next/link';
-import Link from 'next/link';
 
 const Comentario = ({FotoPerfil, NomeUsuario, CreatedAt, NomeProfessor, DisciplinaProfessor, ConteudoComentario, NumeroComentarios}) => {
     return (
