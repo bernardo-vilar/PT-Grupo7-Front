1fe8e3cedb16a20641cb4ea6af66ec7208b4290d
RafaelMkrauss <rafaelmkrauss@gmail.com>
2024-12-16 17:28:43 -0300

    :bug: arrumado bug de tela de professor estar sempre logada

--- my-app/src/app/professor/[id]/page.tsx
+++ my-app/src/app/professor/[id]/page.tsx
@@ -1,11 +1,21 @@
 "use client"
+import { useEffect, useState } from "react";
 import PerfilProfessoresDeslogado from "./PerfilProfessoresDeslogado";
 import PerfilProfessoresLogado from "./PerfilProfessoresLogado";
-const isLoggedIn = true; 
+
 
 
 
 const PerfilProfessores = ({params}) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const storedLoginState = localStorage.getItem("isLoggedIn");
+    if (storedLoginState === "true") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
   const professorId =  params.id;
   return isLoggedIn ? <PerfilProfessoresLogado Id = {professorId}
   /> : <PerfilProfessoresDeslogado 
